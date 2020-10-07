35d577da411e37477b73055316cfa729f7a0f717
Aditya Shrivastav <shriadishri@gmail.com>
2020-10-07 15:40:50 +0530

    newProjectPopup changed to NewProjectPopup in path

--- src/components/DashboardProjectCard.tsx
+++ src/components/DashboardProjectCard.tsx
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import NewProjectPopup from './newProjectPopup';
+import NewProjectPopup from './NewProjectPopup';
 import NewTaskBoardPopup from './NewTaskBoardPopup';
 import './DashboardCard.css';
 const useStyles = makeStyles((theme: Theme) =>
